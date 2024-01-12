461bdb83d3d855312ecdbb52b9b50b54f236cd12
FZFZFZZ <69145190+FZFZFZZ@users.noreply.github.com>
2024-01-12 23:46:46 +0800

    MUI successful

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Button from '@mui/material/Button';
 
-class App extends React.Component {
-	render () {
-		return (
-			<div>Hello Worldds</div>
-		)
-	}
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello Worldds
+        <Button variant="contained" color="primary">
+          Clickk me
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
